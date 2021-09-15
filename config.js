edec2ed315baafbeb0216276308460c9c59c226c
HAMAXx1122 <88665167+HAMAXx1122@users.noreply.github.com>
2021-09-15 14:13:36 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    TOKEN: 'Your bot token here', 
-    YT_API_KEY: 'Youtube API Key here', 
-    prefix: 'prefix here',
-    devs: ['your id/developers here']
+    TOKEN: 'ODg3NjU1NjQzMTA2OTEwMjM4.YUHTvQ.ndf1M5FaMvNBViQpp6CiEpikanw', 
+    YT_API_KEY: '/^(https?:\/\/)?(www\.)?(m\.)?(youtube\.com|youtu\.?be)\/.+$/gi', 
+    prefix: '$',
+    devs: ['840603533144490024']
 }
